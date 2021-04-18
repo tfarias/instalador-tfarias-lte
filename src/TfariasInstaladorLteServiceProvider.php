489cd3668e277ec20c444cd665921c0e1a6e143f
Tiago <tfariasg3@gmail.com>
2021-04-18 18:58:24 -0400

    ajust json

--- src/TfariasInstaladorLteServiceProvider.php
+++ src/TfariasInstaladorLteServiceProvider.php
@@ -16,7 +16,7 @@ class TfariasInstaladorLteServiceProvider extends ServiceProvider
             __DIR__ . '/publishes/partials/lte' => base_path('resources/views/partials/lte'),
             __DIR__ . '/publishes/dist' => base_path('public/lte/dist'),
             __DIR__ . '/publishes/plugins' => base_path('public/lte/plugins'),
-            __DIR__ . '/publishes/permissoes.json' => base_path('database/seeds/data'),
+            __DIR__ . '/publishes/permissoes.json' => base_path('database/seeds/data/permissoes.json'),
         ]);
 
     }
