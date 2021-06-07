8e1206038b38830ac696bd92074f0444e7e47ee7
Tiago <tfariasg3@gmail.com>
2021-06-07 18:57:43 -0400

    feat: button create publishes

--- src/publishes/buttons/lte/create.blade.php
+++
@@ -1,2 +0,0 @@
-<!-- separado pois caso queira aplicar permissões ficará mais fácil -->
-    <a href="{{ route($route) }}" class="btn btn-primary" toggle="tooltip" title="Cadastrar"><i class="fa fa-plus-circle"></i></a>
